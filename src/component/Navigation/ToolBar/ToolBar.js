39c5742206e1591f462d63a7e832d6919dff6b7b
Onamade Samuel <samuelonamade@yahoo.com>
2019-10-30 02:31:22 +0100

    Create ToolBar.js

---
+++ src/component/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import classes from "./ToolBar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+const toolbar = props => (
+  <header className={classes.ToolBar}>
+    <DrawerToggle clicked={props.sideDrawerToggle} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
+export default toolbar;
