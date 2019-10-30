ad2c2c63de05ecb660396eec3cba2996b63c5c05
Onamade Samuel <samuelonamade@yahoo.com>
2019-10-30 03:41:31 +0100

    Update Modal.js

--- src/component/UI/Modal/Modal.js
+++ src/component/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import classes from "./Modal.css";
-import Aux from "../../../hoc/Aux";
+import Auxiliary from "../../../hoc/Auxiliary/Auxiliary";
 import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
@@ -15,7 +15,7 @@ class Modal extends Component {
   }
   render() {
     return (
-      <Aux>
+      <Auxiliary>
         <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
         <div
           style={{
@@ -26,7 +26,7 @@ class Modal extends Component {
         >
           {this.props.children}
         </div>
-      </Aux>
+      </Auxiliary>
     );
   }
 }
