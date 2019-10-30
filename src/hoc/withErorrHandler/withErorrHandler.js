48f6b920e435d7ea20ccb0b1f1646d6af43e08e4
Onamade Samuel <samuelonamade@yahoo.com>
2019-10-30 03:41:22 +0100

    Update withErorrHandler.js

--- src/hoc/withErorrHandler/withErorrHandler.js
+++ src/hoc/withErorrHandler/withErorrHandler.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import Modal from "../../component/UI/Modal/Modal";
-import Aux from "../Aux";
+import Auxiliary from "../Auxiliary/Auxiliary";
 
 const withErrorHandler = (WrapperComponent, axios) => {
   return class extends Component {
@@ -29,12 +29,12 @@ const withErrorHandler = (WrapperComponent, axios) => {
     };
     render() {
       return (
-        <Aux>
+        <Auxiliary>
           <Modal show={this.state.error} modalClosed={this.errorConfirmHandler}>
             {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrapperComponent {...this.props} />
-        </Aux>
+        </Auxiliary>
       );
     }
   };
