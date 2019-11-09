f562451f91249ec42bc5d03cba0ba6c500b34934
Onamade Samuel <samuelonamade@yahoo.com>
2019-11-09 22:08:03 +0100

    Update Orders.js

--- src/container/Orders/Orders.js
+++ src/container/Orders/Orders.js
@@ -22,7 +22,6 @@ class Orders extends Component {
         this.setState({ loading: false, orders: fecthedOrders });
       })
       .catch(err => {
-        //console.log(err);
         this.setState({ loading: false });
       });
   }
